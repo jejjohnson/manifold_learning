0a3b725ad7e6ca46c6f97b85d22bba4114439d4c
jejjohnson <emanjohnson91@gmail.com>
2016-03-28 20:56:56 -0400

    mods

--- manifoldlearning_scikit.py
+++ manifoldlearning_scikit.py
@@ -24,11 +24,16 @@ from sklearn import manifold, datasets
 # generate the dataset 
 
 n_points = 1000                    # number of points
-X, color = datasets.samples_generator.make_s_curve(n_points, 
-                                                   random_state=0)
+X = {}; colors = {}
+data, color = datasets.samples_generator.make_s_curve(n_points,random_state=0)
+X['curve'] = data; colors['scurve'] = color
+del data; del color
+data, color = datasets.samples_generator.make_swiss_roll(n_points,
+                                                         noise=.5,
+                                                         random_state=0)
+X['swissroll'] = data; colors['swissroll'] = color
                                                  
-n_neighbors = 10        # number of neighbours
-n_components = 2        # number of components
+#%%
 #----------
 # plot data
 #----------
@@ -39,12 +44,15 @@ ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.spectral)
 #---------------------
 # Laplacian Eigenmaps
 #---------------------
-t0 = time()
+n_neighbors = 10        # number of neighbours
+n_components = 2        # number of components
+
+t0 = time()             # start timer
 LE = manifold.SpectralEmbedding(n_components=n_components,
                                 affinity='rbf',
                                 n_neighbors=n_neighbors)                
 Y = LE.fit_transform(X)
-t1 = time()
+t1 = time()             # stop timer
 
 print("Laplacian Eigenmaps: {n:.2g} sec".format(n=t1-t0))
 
