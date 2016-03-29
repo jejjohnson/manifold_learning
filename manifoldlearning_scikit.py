a97b6590b5e0fcf1261441a8381e86132ee789ee
jejjohnson <emanjohnson91@gmail.com>
2016-03-29 18:28:39 -0400

    Severed Sphere
    Trying to add in a new dataset called the severed sphere.

--- manifoldlearning_scikit.py
+++ manifoldlearning_scikit.py
@@ -13,12 +13,11 @@ Link: http://goo.gl/ePMFeC
 """
 print(__doc__)
 
+# import necessary packages
 from time import time
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import NullFormatter
-
 from sklearn import manifold, datasets
 
 # generate the dataset 
@@ -26,41 +25,58 @@ from sklearn import manifold, datasets
 n_points = 1000                    # number of points
 X = {}; colors = {}
 data, color = datasets.samples_generator.make_s_curve(n_points,random_state=0)
-X['curve'] = data; colors['scurve'] = color
+X['scurve'] = data; colors['scurve'] = color
 del data; del color
 data, color = datasets.samples_generator.make_swiss_roll(n_points,
                                                          noise=.5,
                                                          random_state=0)
 X['swissroll'] = data; colors['swissroll'] = color
-                                                 
-#%%
-#----------
+
+del data; del color
+import get_data as gt
+data, color = gt.get_severed_sphere(n_points,
+                                    rand_state=0)
+X['sphere'] = data; colors['sphere'] = color
+                                      
+
+#%%----------
 # plot data
 #----------
+                                                 
+dataset = 'sphere'
+
 fig = plt.figure(figsize=(15,8))
 ax = fig.add_subplot(121, projection='3d')
-ax.scatter(X[:,0], X[:,1], X[:,2], c=color, cmap=plt.cm.spectral)
+ax.scatter(X[dataset][:,0], X[dataset][:,1], X[dataset][:,2],
+           c=color,
+           cmap=plt.cm.spectral)
+plt.title("Original Dataset: %s" % dataset)
 
-#---------------------
+#%%---------------------
 # Laplacian Eigenmaps
 #---------------------
+
+# important parameters
 n_neighbors = 10        # number of neighbours
 n_components = 2        # number of components
+kernel = 'nearest_neighbors'          # kernel for affinity matrix
 
 t0 = time()             # start timer
 LE = manifold.SpectralEmbedding(n_components=n_components,
-                                affinity='rbf',
+                                affinity=kernel,
                                 n_neighbors=n_neighbors)                
-Y = LE.fit_transform(X)
+X['transform'] = LE.fit_transform(X[dataset])
 t1 = time()             # stop timer
 
 print("Laplacian Eigenmaps: {n:.2g} sec".format(n=t1-t0))
 
-#---------------
+#%%---------------
 # plot embedding
 #---------------
 ax = fig.add_subplot(122)
-plt.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+plt.scatter(X['transform'][:, 0],
+            X['transform'][:, 1],
+            c=color, cmap=plt.cm.Spectral)
 plt.title("SpectralEmbedding (%.2g sec)" % (t1 - t0))
 ax.xaxis.set_major_formatter(NullFormatter())
 ax.yaxis.set_major_formatter(NullFormatter())
