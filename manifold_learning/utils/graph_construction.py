4d62a4087ff8aa3ef1e9b397cb54298eaf3fd066
Eman Johnson <emanjohnson91@gmail.com>
2016-05-12 17:41:06 -0400

    Incorporated the KnnSolver Class in Schroedinger Eigenmaps algorithm.

--- manifold_learning/utils/graph_construction.py
+++ manifold_learning/utils/graph_construction.py
@@ -8,56 +8,54 @@ import numpy as np
 from scipy.sparse import csr_matrix, csc_matrix, spdiags, diags
 from sklearn.utils.graph import graph_laplacian
 from nearestneighbor_solver import knn_scikit, knn_annoy
+from knn_solvers import KnnSolver
 
 # compute the weighted adjacency matrix
 def compute_adjacency(X, n_neighbors=5, affinity=None,weight='heat',
                       sparse=False, neighbors_algorithm='brute',
-                      metric='euclidean', trees=10, gamma=1.0):
-     
-     
+                      metric='euclidean', trees=10, gamma=1.0,
+                      n_jobs=None):
+
+
      #-----------------------------------
      # K or Approximate Nearest Neighbors
      #-----------------------------------
-     
-     # the scikit learning libraries
-     if neighbors_algorithm in ['brute', 'kd_tree', 'ball_tree']:
-         
-         A_data, A_ind = knn_scikit(X,
-                                    n_neighbors=n_neighbors,
-                                    method=neighbors_algorithm)
-     
-     # the spotify annoy ann library                               
-     elif neighbors_algorithm == 'annoy':
-         
-         A_data, A_ind = knn_annoy(X, metric=metric,
-                                   n_neighbors=n_neighbors,
-                                   trees=trees)
-                         
-     else:
-         raise ValueError('Sorry. Unrecognized knn algorithm.')
-     
+
+     # initialize knn model with available parameters
+     knn_model = KnnSolver(n_neighbors=n_neighbors,
+                           nn_algorithm=neighbors_algorithm,
+                           n_jobs=n_jobs,
+                           affinity=affinity,
+                           weight=weight,
+                           gamma=gamma,
+                           trees=trees,
+                           metric=metric)
+
+     # find the nearest neighbor indices and distances
+     A_data, A_ind = knn_model.find_knn(X)
+
      #---------------------------------
      # Adjacency matrix and Data Kernel
      #---------------------------------
-     
+
      # start constructing the adjacency matrix
      W = create_adjacency(A_data, A_ind)
-     
+
      if weight == 'connectivity':
-         raise ValueError('Sorry. Connectivity currently fails.')             
+         raise ValueError('Sorry. Connectivity currently fails.')
          W.data = 1
-         
-         
+
+
      elif weight == 'heat':
          W.data = np.exp(-W.data**2 / gamma**2)
-        
-         
+
+
      elif weight == 'angle':
          W.Data = np.exp(-np.arccos(1-W.data))
-         
+
      else:
          raise ValueError('Sorry. Unrecognized affinity weight')
-     
+
      return W
 
 
@@ -97,15 +95,15 @@ def create_adjacency(distance_vals, indices):
 
     # Make sure the matrix is symmetric
     W_sparse_symmetric = maximum(W_sparse, W_sparse.T)
-    
+
     return W_sparse_symmetric
 
 
 # Find the maximum elements between two sparse matrices
 def maximum(A,B):
-    """This gives you the element-wise maximum between two sparse 
+    """This gives you the element-wise maximum between two sparse
     matrices of size (nxn)
-    
+
     Reference
     ---------
         http://goo.gl/k0Yfmk
@@ -115,7 +113,7 @@ def maximum(A,B):
     return A - A.multiply(BisBigger) + B.multiply(BisBigger)
 
 # Find the Laplacian Matrix from an Adjacency Matrix
-def create_laplacian(Adjacency, 
+def create_laplacian(Adjacency,
                      norm_lap=None,
                      method='Personal',
                      sparse=None):
@@ -130,18 +128,18 @@ def create_laplacian(Adjacency,
     * Laplacian       - an NxN laplacian array
     * Diagonal        - an NxN diagonal array
     """
-    
+
     if method in ['personal', 'Personal']:
-        
+
         D = spdiags(data=np.squeeze(np.asarray(Adjacency.sum(axis=1))),
                     diags=[0],
                     m=Adjacency.shape[0],
                     n=Adjacency.shape[0])
         return D-Adjacency, D
-        
-        
+
+
     elif method in ['sklearn', 'scikit']:
-        
+
         L, D = graph_laplacian(Adjacency, normed=norm_lap,
                                return_diag=True)
         D = spdiags(data=D,
@@ -149,43 +147,43 @@ def create_laplacian(Adjacency,
                     m=Adjacency.shape[0],
                     n=Adjacency.shape[0])
         return L, D
-        
-    else: 
+
+    else:
         raise ValueError('Unrecognized Graph Laplacian method'
         'construction.')
 
 
-                    
+
 # create feature based matrix
 def create_feature_mat(X,A, sparse=None):
     """This is the feature-based matrix of the form:
         X^T A X and X^T B X
-    
+
     Parameters
     ----------
     X : (nxm) array
-        This array has n_data points by m_features. Typically a data 
+        This array has n_data points by m_features. Typically a data
         matrix of n samples and m features.
     A : (nxn) array
         This array has n_data points by n_data points. This is typically
         constructed as an Laplacian matrix, adjacency matrix or diagonal
         degree matrix.
-  
-    
+
+
     Returns:
     --------
-    
+
     X^TAX : (mxm) array
         This array has m_features by m_features.
     """
     if sparse:
-        
+
         return X.T.dot(A.dot(X))
-    
+
     else:
-        try: 
+        try:
             return np.dot(X.T, np.dot(A.toarray(),X))
-            
+
         except:
             return np.dot(X.T, np.dot(A,X))
 def laplacian_test():
@@ -196,7 +194,7 @@ def laplacian_test():
     import matplotlib.pyplot as plt
 #    plt.style.use('ggplot')
 
-    
+
     A = csr_matrix(make_spd_matrix(100))
 
     L, D, t = [], [], []
@@ -206,40 +204,31 @@ def laplacian_test():
         t1 = time.time()
         L.append(temp_L); D.append(temp_D)
         t.append(t1-t0)
-    
-    
-    
-    fig, ax = plt.subplots(nrows=1, ncols=2)
-    
 
-    
+
+
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+
+
+
     ax[0].spy(L[0], precision=1E-10, markersize=.2)
     ax[0].set_title('My Method; {t:.2e} secs'.format(t=t[0]))
     ax[1].spy(L[1], precision=1E-10, markersize=.2)
     ax[1].set_title('Sklearn; {t:.2e} secs'.format(t=t[1]))
-    
+
     print np.shape(L[0]), np.shape(L[1])
     tol = 1E-12
     print('Different between the Laplacian Matrix' \
             'values close with tol: {tol}?'.format(tol=tol))
-    
+
     assert (np.allclose(L[0].data, L[1].data)), "False Laplacians not" \
     "the same."
     print('Test passed.')
 
-    
+
     plt.show()
-    
-    
+
+
 if __name__ == "__main__":
+    # sanity test
     laplacian_test()
-    
-
-
-
-    
-
-    
-#    L, D = create_laplacian(A, norm_lap=None,
-#                            method='Personal',
-#                            sparse=None)
