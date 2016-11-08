fb573adb4a7ac0fc83739613dbf01905fb794a35
Eman Johnson <emanjohnson91@gmail.com>
2016-11-08 14:00:49 -0500

    Update lapalcian.m
    Updated the laplacian.m function with the appropriate laplacian matrix calculations. Implemented the unnormalized case and the normalized Shi, Malik case. The Jordan, Weiss case is a bit more counter intuitive and requires more fidgeting with the emebdding eigenvectors. I also still need to do the input parsing.

--- src/matlab/main_functions/helper_functions/laplacian.m
+++ src/matlab/main_functions/helper_functions/laplacian.m
@@ -6,6 +6,11 @@ LAPLACIAN computs the laplacian matrix of an adjacency matrix, W.
 Examples
 --------
 
+>> Options.lapType = 'unnormalized';
+>> L = laplacian(L, Options);
+
+>> [L, D] = laplacian(L, Options);
+
 Parameters
 ----------
 * W   - array (N x N)
@@ -15,14 +20,17 @@ Parameters
       + lapType - str, 
             ~ 'unnormalized' (default)
             ~ 'normalized'
-            ~ 'geometric'
-            ~ 'randomwalk'
-            ~ 'symmetric'
-            ~ 'renormalized'
+            ~ 'geometric' (TODO)
+            ~ 'randomwalk' (TODO)
+            ~ 'symmetric' (TODO)
+            ~ 'renormalized' (TODO)
       + matType - str, ['dense', 'sparse']
 Returns
 -------
-
+* Laplacian - array (N x N)
+                an array representing the Laplacian matrix
+* Degree  - array (N x N)
+            an array representing the diagonal degree matrix 
 
 Information
 -----------
@@ -36,9 +44,11 @@ Date      : 11/8/16
 %-- Degree Matrix --%
 
 % Calculate the degree matrix
-degreeMat = sum(W, 2);
-D = sparse(1:size(W,1), 1:size(W,2), degreeMat);
+degW = sum(W, 2);
+D = sparse(1:size(W,1), 1:size(W,2), degW);
 
+% Avoid dividing by zero
+degW(degW == 0) = eps;
 
 %=====================================================================
 %-- Laplacian Matrix --%
@@ -47,6 +57,29 @@ D = sparse(1:size(W,1), 1:size(W,2), degreeMat);
 switch laptype
     
     case 'unnormalized'
+          
+          % Calculate the Normalized Laplacian Matrix
+          L = D - W;
+    
+    case 'normalized'         % shi, malik
+          
+          % Calculate the inverse of D
+          D = spdiags(1 ./ degW, 0 size(D, 1), size(D, 2));
+          
+          % Calculate the normalized Laplacian Matrix
+          L = D * L
+          
+    case 'normalized'          % jordan, weiss
+    
+          % Calculate D^(-1/2)
+          D = spdiags(1./(degW .^ 0.5), 0, size(D,1), size(D,2));
+          
+          % Calculate the normalized Laplacian
+          L = D * L * D;
+    otherwise
+          error([mfilename, 'laplacian:badlapchoice'], ...
+               'Unrecognized Laplacian matrix choice.');
+end
 
 
 
