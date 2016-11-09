ad0da1546f4931f40f7ca1131db13c75d0e07627
jemanjohnson <emanjohnson91@gmail.com>
2016-11-08 22:23:08 -0500

    Updated laplacian.m
    Cleaned it up and added the output parser.

--- src/matlab/main_functions/helper_functions/laplacian.m
+++ src/matlab/main_functions/helper_functions/laplacian.m
@@ -1,6 +1,6 @@
 function varargout = laplacian(W, varargin)
 %{
-======================================================================
+===========================================================================
 LAPLACIAN computs the laplacian matrix of an adjacency matrix, W.
 
 Examples
@@ -37,10 +37,17 @@ Information
 Author    : J. Emmanuel Johnson
 Email     : emanjohnson91@gmail.com
 Date      : 11/8/16
-======================================================================
+===========================================================================
 %}
 
-%=====================================================================
+%==========================================================================
+%-- CHECK INPUTS --%
+
+narginchk(0,1);
+
+nargoutchk(1,2);
+
+%==========================================================================
 %-- Degree Matrix --%
 
 % Calculate the degree matrix
@@ -50,7 +57,7 @@ D = sparse(1:size(W,1), 1:size(W,2), degW);
 % Avoid dividing by zero
 degW(degW == 0) = eps;
 
-%=====================================================================
+%==========================================================================
 %-- Laplacian Matrix --%
 
 % Compute the Laplacian Matrix
@@ -61,36 +68,59 @@ switch laptype
           % Calculate the Normalized Laplacian Matrix
           L = D - W;
     
-    case 'normalized'         % shi, malik
+    case 'normalizedsm'         % shi, malik
+
+        % Calculate the Normalized Laplacian Matrix
+        L = D - W;
           
-          % Calculate the inverse of D
-          D = spdiags(1 ./ degW, 0 size(D, 1), size(D, 2));
+        % Calculate the inverse of D
+        D = spdiags(1 ./ degW, 0, size(D, 1), size(D, 2));
+
+        % Calculate the normalized Laplacian Matrix
+        L = D * L;
+
+        % NEED TO DO THIS FOR JORDAN WEISS
+
+        % Compute Eigenvectors corresponding to the k smallest eigenvalues
+        %diff = eps;
+        %[U, ~] = eigs(L, k, diff);
+        %
+        %% Normalized the eigenvectors row-wise
+        %U = bsxfun(@divide, U, sqrt(U .^2, 2));
           
-          % Calculate the normalized Laplacian Matrix
-          L = D * L
-          
-          % NEED TO DO THIS FOR JORDAN WEISS
-          
-          % Compute Eigenvectors corresponding to the k smallest eigenvalues
-          %diff = eps;
-          %[U, ~] = eigs(L, k, diff);
-          %
-          %% Normalized the eigenvectors row-wise
-          %U = bsxfun(@divide, U, sqrt(U .^2, 2));
-          
-    case 'normalized'          % jordan, weiss
-    
-          % Calculate D^(-1/2)
-          D = spdiags(1./(degW .^ 0.5), 0, size(D,1), size(D,2));
-          
-          % Calculate the normalized Laplacian
-          L = D * L * D;
+    case 'normalizedjw'          % jordan, weiss
+        
+        % Calculate the Normalized Laplacian Matrix
+        L = D - W;
+        
+        % Calculate D^(-1/2)
+        D = spdiags(1./(degW .^ 0.5), 0, size(D,1), size(D,2));
+
+        % Calculate the normalized Laplacian
+        L = D * L * D;
     otherwise
           error([mfilename, 'laplacian:badlapchoice'], ...
                'Unrecognized Laplacian matrix choice.');
 end
 
+%==========================================================================
+%-- VARIABLE OUTPUT --%
 
+switch numel(nargout)
+    
+    case 1
+        
+        varargout{1} = L;
+        
+    case 2
+        
+        varargout{1} = L;
+        varargout{2} = D;
+        
+    otherwise
+        error([mfilename, 'laplacian:badnumoutputs'], ...
+            'Invalid number of output variables.');
+end
 
 
 end
